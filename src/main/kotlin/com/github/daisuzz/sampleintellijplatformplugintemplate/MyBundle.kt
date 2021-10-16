0eb157b0ae9d9d161f79d4e13b6b96fe82366859
GitHub Action <action@github.com>
2021-10-16 14:47:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/daisuzz/sampleintellijplatformplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/daisuzz/sampleintellijplatformplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.daisuzz.sampleintellijplatformplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/daisuzz/sampleintellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.daisuzz.sampleintellijplatformplugintemplate.services
+
+import com.github.daisuzz.sampleintellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
